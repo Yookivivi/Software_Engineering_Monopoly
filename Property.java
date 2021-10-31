9ac9b07fb5ee819bd287a5480e1b498f77688064
Dylan0211 <54855930+Dylan0211@users.noreply.github.com>
2021-10-31 12:47:04 +0800

    Update Property.java

--- Property.java
+++ Property.java
@@ -2,8 +2,11 @@ public class Property {
     private int landNum;//the number of lands
     private int[] lands;// the indexes of lands owned by the player
 
-    public void updateProperty(){};//change the property list
+    public void updateProperty(int mode, int position){};//change the property list
     public void printProperty(){};//print the property list
+    public int getLandNum(){
+        return landNum;
+    };
     public int[] getProperty(){
         return lands;
     };
