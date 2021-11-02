539d8e23673cab5652f9b33900adc1e05618dd4f
Mars4747 <74805955+Mars4747@users.noreply.github.com>
2021-11-03 01:50:29 +0800

    Update Dice.java

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,6 +1,7 @@
 public class Dice {
     public int dice1;// the first 4 faces dice
     public int dice2;// the second 4 faces dice
+    public int totalDice;
 
     public void rollDice(){};// roll the dices
 }
