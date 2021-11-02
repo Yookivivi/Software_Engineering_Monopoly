417527e3ae312c57c47fb27595f8ff0a52a6e828
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-02 21:16:10 +0800

    Update GameTest.java

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.junit.jupiter.api.*;
 import java.io.*;
 
@@ -7,35 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
     
     Game game;
+    public void setInput(String input){
+        ByteArrayInputStream in;
+        in=new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
 
     @BeforeEach
     void setUp(){
         game = new Game();
     }
 
-    //不太确定这里
     @Test
     @DisplayName("Test if user could correctly start game")
     public void testStartGame(){
         game.startGame();
-        assertNotNull(game.board, "Game has created the board.");
-        assertNotNull(game.players[0], "Game has been included at least 1 player.");
-        assertEquals(game.playerNum, game.players.length, "All the players have been added in this game.");
+        assertNotNull(game.board, "Game should create the board.");
+        assertNotNull(game.players[0], "Game should include at least 1 player.");
+        assertEquals(game.playerNum, game.players.length, "All the players shold be added in this game.");
         for (int i = 0; i < game.playerNum; i++) {
             assertEquals(i+1, game.currentPlayer[i], "All the players have been included in this game.");
         }
 
     }
 
-    @Test
-    @DisplayName("Test if user could correctly start game")
-    public void testLoadGame(){
-    }
 
     @Test
-    @DisplayName("Test if user could correctly start game")
-    public void testSaveGame(){
+    @DisplayName("Test if the player could be correctly added")
+    public void testAddPlayer(){
+        int id=1;
+        String name="Alice\n";
+        game.addNewPlayer(id);
+        setInput(name);
+        assertEquals(id, game.players[0].getId(),"The first player should be correctly added");
+        assertEquals(name, game.players[0].getName(),"The first player should be correctly added");
     }
+
 
     @Test
     @DisplayName("Test if the game could ask the players take turns to play the game.")
@@ -44,7 +51,7 @@ public class GameTest {
         for(int i=0; i<50; i++){
             if(!game.isEnd){
                 game.takeTurn();
-                assertEquals(i % (game.currentPlayer.length) + 1, game.currentRound);
+                assertEquals(i % (game.currentPlayer.length) + 1, game.currentRound,"The current round should be "+game.currentRound);
             }
         }
     }
@@ -54,10 +61,10 @@ public class GameTest {
     public void testJudgeIsEnd(){
         game.judgeIsEnd();
         if(game.currentPlayer.length==1 || game.currentRound==100){
-            assertTrue(game.isEnd);
+            assertTrue(game.isEnd, "Game should be end");
         }
         else{
-            assertFalse(game.isEnd);
+            assertFalse(game.isEnd, "Game should be continue");
         }
     }
 
@@ -70,7 +77,8 @@ public class GameTest {
             ByteArrayOutputStream outContent = new ByteArrayOutputStream();
             System.setOut(new PrintStream(outContent));
             if(game.currentPlayer.length==1){
-                assertEquals("The winner is "+game.players[game.currentPlayer[0]], outContent.toString());
+                assertEquals("The winner is "+game.players[game.currentPlayer[0]], outContent.toString(),
+                        "The winner of this game should be "+game.players[game.currentPlayer[0]]);
             }
             else{
                 String winner=game.players[game.currentPlayer[0]].getName();
