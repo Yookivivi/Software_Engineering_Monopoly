e278bd1e30212634a0f1d02a60aa7b5ac01147a6
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-02 21:23:33 +0800

    Update Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,6 +8,7 @@ public class Game {
 
     public Game(){};//constructor
     public void startGame(){};//to start a new game
+    public void addNewPlayer(int id){};//to create the player and add him/her to the players
     public void loadGame(){};//to load a game
     public void saveGame(){};//to save a game
     public void takeTurn(){};//to do the next turn
