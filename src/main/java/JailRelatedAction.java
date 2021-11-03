42654869dafdd5fc4713d0ed23ee6135de46692f
Mars4747 <74805955+Mars4747@users.noreply.github.com>
2021-11-03 15:34:27 +0800

    Update JailRelatedAction.java

--- src/main/java/JailRelatedAction.java
+++ src/main/java/JailRelatedAction.java
@@ -1,4 +1,7 @@
 public class JailRelatedAction extends ActionController {
     private boolean rollDouble;
     public void updateInJail(Player p) {}// update the players' state of inJail
+    public boolean getrollDouble(Dice d){
+        return rollDouble;
+    }
 }
