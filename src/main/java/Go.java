fb550be70db640900f6c2eb81866a9acdc280d16
Dylan0211 <54855930+Dylan0211@users.noreply.github.com>
2021-11-12 16:25:50 +0800

    Update and rename GO.java to Go.java

--- src/main/java/Go.java
+++ src/main/java/Go.java
@@ -1,6 +1,6 @@
-public class GO extends MoneyRelatedSquare{
+public class Go extends MoneyRelatedSquare{
     public int award; // the money for players, normally = 1500
 
-    public GO(){};
+    public Go(){};
     public void updateMoney(Player p){};// update players money
 }
