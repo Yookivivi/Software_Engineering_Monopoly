57b21a973c35499b224073aea84a2aed8ca20693
Mars4747 <74805955+Mars4747@users.noreply.github.com>
2021-11-14 00:06:15 +0800

    Update Dice.java

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,7 +1,15 @@
+import java.util.Random;
+
 public class Dice {
     public int dice1;// the first 4 faces dice
     public int dice2;// the second 4 faces dice
     public int totalDice;
 
-    public void rollDice(){};// roll the dices
+    public void rollDice(){
+        Random d1 = new Random();
+        dice1 = d1.nextInt(4)+1;
+        Random d2 = new Random();
+        dice2 = d2.nextInt(4)+1;
+        totalDice = dice1 + dice2;
+    };// roll the dices
 }
