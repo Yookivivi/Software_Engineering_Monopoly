f67e8ed0474f73897374bbcf8847acc8982ba624
Mars4747 <74805955+Mars4747@users.noreply.github.com>
2021-11-14 02:22:45 +0800

    Update PropertyRelatedActionTest.java

--- src/test/java/PropertyRelatedActionTest.java
+++ src/test/java/PropertyRelatedActionTest.java
@@ -20,34 +20,32 @@ class PropertyRelatedActionTest {
     @DisplayName("Test buy land and update property list")
     void buylandTest() {
         //set property of player 1
-        p1.setPropertyList(pro);
+        p1.setPropertyList(1,7);
         //add original property
         pro.updateProperty(1,2);
         pro.updateProperty(1,3);
         //a player is at position 5 and buy the land
         l1.position = 5;
         p1.setPosition(l1.position);
-        y1.updatePosition(p1,l1.position);
-        y1.buyland(p1);
+        y1.buyland(p1,l1);
         y1.updateProperty(p1);
         Assertions.assertEquals(p1.getId(),l1.getOwner());
-        Assertions.assertEquals(pro,p1.getPropertyList());
+        Assertions.assertSame(pro,p1.getPropertyList());
     }
 
     @Test
     void updatePropertyTest() {
         //set property of player 2
-        p2.setPropertyList(pro2);
+        p2.setPropertyList(1,2);
         //add original property
         pro2.updateProperty(1,7);
         pro2.updateProperty(1,8);
         // player 2 is at position 10
         l2.position = 10;
         p2.setPosition(l2.position);
-        y2.updatePosition(p2,l2.position);
-        y2.buyland(p2);
+        y2.buyland(p2, l2);
         y2.updateProperty(p2);
         Assertions.assertEquals(p2.getId(),l2.getOwner());
-        Assertions.assertEquals(pro2,p2.getPropertyList());
+        Assertions.assertSame(pro2,p2.getPropertyList());
     }
 }
