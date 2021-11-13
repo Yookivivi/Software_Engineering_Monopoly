78cb0f4086b414180fc2cf2aba57d4342456e56b
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-13 21:25:06 +0800

    Update Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,8 @@
+package main.java;
+
+
+import java.util.Scanner;
+
 public class Game {
     public int playerNum; //the number of players
     public int currentRound; //current round
@@ -6,12 +11,45 @@ public class Game {
     public Player[] players; //an array for all the players
     public Board board;//game board
 
-    public Game(){};//constructor
-    public void startGame(){};//to start a new game
-    public void addNewPlayer(int id){};//to create the player and add him/her to the players
+    public Game(){
+
+    };//constructor
+
+    public void startGame(int input_num){
+        board=new Board();
+        playerNum=input_num;
+    };//to start a new game
+
+    public void addNewPlayer(int id, String name){
+        Player player=new Player(id, name);
+        players[id-1]=player;
+        currentPlayer[id-1]=id;
+    };//to create the player and add him/her to the players
+
     public void loadGame(){};//to load a game
+
     public void saveGame(){};//to save a game
-    public void takeTurn(){};//to do the next turn
-    public void judgeIsEnd(){};// to judge whether the game is ended
-    public void printWinner(){};// print the winner of the game
+
+    public void takeTurn(int i){
+        Player player=players[currentPlayer[i]];
+
+
+    };//to do the next turn
+    public void judgeIsEnd(){
+        if(currentPlayer.length==1){
+            isEnd=true;
+        }
+        else if(currentRound==100){
+            isEnd=true;
+        }
+    };// to judge whether the game is ended
+
+    public String[] printWinner(){
+        int winner_num=currentPlayer.length;
+        String[] winners=new String[winner_num];
+        for(int i=0; i<winner_num; i++){
+            winners[i]=players[currentPlayer[i]-1].getName();
+        }
+        return winners;
+    };// print the winner of the game
 }
