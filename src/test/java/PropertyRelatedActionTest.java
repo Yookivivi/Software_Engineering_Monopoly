a531c16876c89888663a281428fbaadca7fbd892
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-14 17:44:19 +0800

    帮小马更新

--- src/test/java/PropertyRelatedActionTest.java
+++ src/test/java/PropertyRelatedActionTest.java
@@ -1,9 +1,9 @@
-import Model.Property;
-import Model.PropertyRelatedAction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import Model.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class PropertyRelatedActionTest {
     private Player p1;
     private Player p2;
@@ -15,41 +15,31 @@ class PropertyRelatedActionTest {
     private LandSquare l2;
 
 
-/*
+
     @Test
     @DisplayName("Test buy land and update property list")
     void buylandTest() {
-        //set property of player 1
-        //??????
-        p1.setPropertyList(pro);
-        //add original property
-        pro.updateProperty(1,2);
-        pro.updateProperty(1,3);
+        p1.getPropertyList().updateProperty(1,2);
+        p1.getPropertyList().updateProperty(1,3);
         //a player is at position 5 and buy the land
         l1.position = 5;
         p1.setPosition(l1.position);
         y1.buyland(p1,l1);
         y1.updateProperty(p1,l1);
         Assertions.assertEquals(p1.getId(),l1.getOwner());
-        Assertions.assertSame(pro,p1.getPropertyList());
+        Assertions.assertEquals(5,p1.getProperty()[2]);
     }
 
     @Test
     void updatePropertyTest() {
-        //set property of player 2
-        //?????
-        p2.setPropertyList(pro2);
-        //add original property
-        pro2.updateProperty(1,7);
-        pro2.updateProperty(1,8);
+        p2.getPropertyList().updateProperty(1,7);
+        p2.getPropertyList().updateProperty(1,8);
         // player 2 is at position 10
         l2.position = 10;
         p2.setPosition(l2.position);
         y2.buyland(p2, l2);
         y2.updateProperty(p2,l2);
         Assertions.assertEquals(p2.getId(),l2.getOwner());
-        Assertions.assertSame(pro2,p2.getPropertyList());
+        Assertions.assertSame(pro2,p2.getProperty()[2]);
     }
-
- */
 }
