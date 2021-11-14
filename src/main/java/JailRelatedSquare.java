a1217f0294bc14b86896e761ff22456a1093bde8
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-14 16:26:22 +0800

    Delete src directory

--- src/main/java/JailRelatedSquare.java
+++
@@ -1,3 +0,0 @@
-abstract public class JailRelatedSquare extends Square {
-    public void updateInJail(Player p){};// change player's state of inJail
-}
--- src/main/java/MoneyRelatedSquare.java
+++
@@ -1,6 +0,0 @@
-abstract class MoneyRelatedSquare extends Square{
-    /**
-     * update the player's Money
-     */
-    public void updateMoney(Player p){}; // change the player's money
-}
