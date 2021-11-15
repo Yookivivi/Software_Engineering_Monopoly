6ffde4b7914dbc571a191706e35ca2af643a0019
Maxwell <54855930+Dylan0211@users.noreply.github.com>
2021-11-16 00:26:04 +0800

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import Controller.*;
+import Model.*;
+import View.*;
+
+import java.io.File;
+
+
+public class Main {
+    public static void main(String[] args){
+        GameController ctrl = new GameController();
+        GameView view = new GameView();
+
+        File save = new File("save"); // create save directory
+        if (!save.exists()) {
+            boolean b = save.mkdir();
+            while(!b){
+                b = save.mkdir();
+            }
+        }
+
+        while(true){
+            view.printWelcomeMessage();
+            view.printChooseModeMessage();
+            String input_num = ctrl.chooseBeginWay();
+            GameController controller;
+            if (input_num.equals("1")){ // start a new game
+                Game game = new Game();
+                controller = new GameController(game);
+                controller.startGameController();
+            }
+            else{ // load a game
+                controller = new GameController(ctrl.loadGameController());
+            }
+            controller.takeTurnController();
+            if (controller.gameIsEnd()){
+                String choice = controller.endGame();
+                if (choice.equals("e")) break;
+            }
+        }
+    }
+}
