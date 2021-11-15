2eeb247d4967d4da552e0b36032b197bb374e9ff
Maxwell <54855930+Dylan0211@users.noreply.github.com>
2021-11-16 00:24:46 +0800

    Update GameController.java

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -3,19 +3,40 @@ import Model.*;
 
 import View.GameView;
 //import main.java.Game;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class GameController {
     private Game game;
-    private GameView gameView;
+    private GameView gameView = new GameView();
 
-    public GameController(Game game, GameView gameView){
+    public GameController(Game game){
         this.game = game;
-        this.gameView = gameView;
+    }
+    public GameController(){}
+
+    public boolean gameIsEnd(){
+        return game.isEnd;
+    }
+
+    public String endGame(){
+        gameView.printContinueMessage();
+        Scanner s = new Scanner(System.in);
+        String choice = s.toString();
+        while(!choice.equals("m") && !choice.equals("e")){
+            gameView.printInvalidChoiceMessage();
+            s = new Scanner(System.in);
+            choice = s.toString();
+        }
+        if (choice.equals("e")){
+            gameView.printEndGameMessage();
+        }
+        return choice;
     }
 
     // 11/14/21:58
-    public void chooseBeginWay(){//choose the way that begin game
+    public String chooseBeginWay(){//choose the way that begin game
         gameView.printChooseModeMessage();
         Scanner S=new Scanner(System.in);
         String input_num=S.toString();
@@ -24,12 +45,7 @@ public class GameController {
             S=new Scanner(System.in);
             input_num=S.toString();
         }
-        if(input_num.equals("1")){
-            startGameController();
-        }
-        else{
-            loadGameController();
-        }
+        return input_num;
     }
 
     public void startGameController(){
@@ -53,20 +69,19 @@ public class GameController {
             game.addNewPlayer(i+1, name);
             gameView.printSuccessfullyAddNewPlayerMessage(game.currentPlayer[i], game.players[i].getName());
         }
-        takeTurnController();
     }
 
     // 11/14/21:49
-    public void saveGameController(){
-        gameView.printSaveGaveMessage();
+    public int saveGameController(){ // return 0: exit 1: continue
+        gameView.printTurnEndMessage();
         Scanner S=new Scanner(System.in);
         String choice=S.toString();
-        while(!choice.equals("y") && !choice.equals("n")){
+        while(!choice.equals("s") && !choice.equals("e") && !choice.equals("c")){
             gameView.printInvalidChoiceMessage();
             S = new Scanner(System.in);
             choice = S.toString();
         }
-        if(choice.equals("y")){
+        if(choice.equals("s")){ // save game
             gameView.printSaveNameMessage();
             S=new Scanner(System.in);
             String name=S.toString(); // name should not include format (e.g. .txt)
@@ -90,6 +105,13 @@ public class GameController {
             else{
                 game.saveGame(name);
             }
+            return 1;
+        }
+        else if (choice.equals("e")){ // exit game
+            return 0;
+        }
+        else{ // continue
+            return 1;
         }
     }
 
@@ -116,24 +138,23 @@ public class GameController {
         return game.loadGame(fs[choice-1]);
     }
 
-
     // new
     public void takeTurnController(){
         while(!game.isEnd){
             int current_playerNum=game.currentPlayer.length;
-            for(int i=0; i<current_playerNum; i++){
+            for(int i=0; i<current_playerNum; i++){ // how to keep track of current player
                 gameView.printTakeTurnMessage(game.currentRound,game.players[game.currentPlayer[i]-1].getId(),game.players[game.currentPlayer[i]-1].getName());
                 game.takeTurn(i);
-                saveGameController();
+                int exit = saveGameController();
+                if (exit == 0) break;
                 game.judgeIsEnd();
                 if(game.isEnd){
                     break;
                 }
-
             }
             game.currentRound++;
         }
-        printWinnerController();
+        if (game.isEnd) printWinnerController();
     }
 
     public void printWinnerController(){
