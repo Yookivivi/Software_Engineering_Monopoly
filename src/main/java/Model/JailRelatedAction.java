520cef0a888c0848467865ee242cdafee3fa41b1
Mars4747 <74805955+Mars4747@users.noreply.github.com>
2021-11-17 01:23:00 +0800

    Add files via upload

--- src/main/java/Model/JailRelatedAction.java
+++ src/main/java/Model/JailRelatedAction.java
@@ -2,6 +2,14 @@ package Model;
 
 public class JailRelatedAction extends ActionController {
     private boolean rollDouble;
+    private Player p;
+    private Dice dice;
+
+    public JailRelatedAction(Player player) {
+        super(player);
+        this.dice = new Dice();
+    }
+
 
     public boolean getrollDouble(Dice d){
         if (d.dice1 == d.dice2) {
@@ -15,13 +23,12 @@ public class JailRelatedAction extends ActionController {
 
 
     // update the players' state of inJail
-    public void updateInJail(Player p) {
+    public void updateInJail_R(Player p) {
         int fine = 150;
 
         //if player throw doubles, she can immediately get out of jail.
         if(getrollDouble(currentDice)){
             p.setInJail(false);
-            //?
             updatePosition(p,p.getPosition()+ currentDice.totalDice);
         }
         //judge the round in jail
@@ -29,10 +36,17 @@ public class JailRelatedAction extends ActionController {
             //if the player does not throw doubles by her third turn, she must pay fine and then get out of jail
             if(p.getInJailRound() == 3){
                 p.setInJail(false);
-                updateMoney(p,p.getMoney()-fine);
+                updateMoney(p,-fine);
                 updatePosition(p,p.getPosition()+ currentDice.totalDice);
             }
             p.setInJailRound(p.getInJailRound()+1);
+            p.setInJail(true);
         }
+    }
+
+    public void updateInJail_P(Player p){
+        int fine = 150;
+        p.setInJail(false);
+        updateMoney(p,-fine);
     }
 }
