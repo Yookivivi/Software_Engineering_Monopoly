04cb2946202be3b1a437ed3798985956ad8c0c82
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-17 00:46:36 +0800

    Update PlayerController.java

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -16,9 +16,17 @@ public class PlayerController {
     }
 
 
-    public void updateIsOut(){
+    public void updateIsOut(Board board){
         if (player.getMoney()<=0){
             player.setIsOut(true);
+            Property property=player.getPropertyList();
+            int propertyNum=player.getPropertyList().getLandList().size();
+            for(int i=0; i<propertyNum; i++){
+                int position=player.getPropertyList().getLandList().get(0);
+                //LandSquare landSquare=
+                property.updateProperty(0,player.getPropertyList().getLandList().get(0));
+
+            }
 
         }
 
