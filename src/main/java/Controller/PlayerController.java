18a93deb25428d742b7eec993bbf8ca23b6804dd
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2021-11-18 00:25:21 +0800

    comment

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -13,15 +13,27 @@ public class PlayerController {
         this.board=board;
     }
 
-    public void startTurnController(){
+    public void startTurnController(){//control the condition that player is in jail and not in jail
         playerView.printPlayerTurnMessage(player.getId(),player.getName());
-        if(player.getInJail()){
+        if(player.getInJail()){//in jail
             playerView.printInJailMessage(player.getInJailRound());
             JailRelatedActionController jailRelatedActionController=new JailRelatedActionController(player);
-            jailRelatedActionController.chooseHowToGetOut();
+            jailRelatedActionController.chooseHowToGetOut();//choose the way to get out of the jail
             if (jailRelatedActionController.choice==2){//player choose to pay money
-                updateIsOut();
-                if(!player.getIsOut()) notInJailActionController();
+                updateIsOut();//judge if the player is out
+                if(!player.getIsOut()) notInJailActionController();//not out, then go to roll the dice
+            }
+            else{
+                if(!player.getInJail()){//go out by throw double
+                    int position=player.getPosition();
+                    Square square=board.squares[position-1];
+                    playerView.printUpdatedSquareMessage(position,square.getName());
+                    if (square.takeEffect(player)==1){//land on an unowned land
+                        LandSquare landSquare=(LandSquare) square;
+                        PropertyRelatedActionController p=new PropertyRelatedActionController(player);
+                        p.choosebuyland(landSquare);//ask if buy land
+                    }
+                }
             }
 
         }
@@ -30,7 +42,7 @@ public class PlayerController {
         }
     }
 
-    public void notInJailActionController(){
+    public void notInJailActionController(){//not in jail
         ActionController actionController=new ActionController(player);
         actionController.currentDice.rollDice();
         int dice1= actionController.currentDice.dice1;
@@ -38,34 +50,34 @@ public class PlayerController {
         int total_dice=actionController.currentDice.totalDice;
         playerView.printDiceMessage(dice1, dice2, total_dice);
         int position=player.getPosition()+total_dice;
-        if(position>20){
+        if(position>20){//after one circle, go back to the first square and start again
             position-=20;
-            player.setMoney(player.getMoney()+1500);
+            player.setMoney(player.getMoney()+1500);//pass go, add money
             System.out.println(player.getName()+" has passed the GO, and got HKD1500");
         }
-        actionController.updatePosition(player,position);
-        updateSquareController(position);
+        actionController.updatePosition(player,position);//update the position
+        updateSquareController(position);//get the action of this position
     }
 
-    public void updateSquareController(int position){
+    public void updateSquareController(int position){//control the action on the current square
         Square square=board.squares[position-1];
         playerView.printUpdatedSquareMessage(position,square.getName());
-        if (square.takeEffect(player)==1){
+        if (square.takeEffect(player)==1){//land on an unowned land
             LandSquare landSquare=(LandSquare) square;
             PropertyRelatedActionController p=new PropertyRelatedActionController(player);
-            p.choosebuyland(landSquare);
+            p.choosebuyland(landSquare);//ask if buy land
         }
         updateIsOut();
     }
 
 
-    public void updateIsOut(){
+    public void updateIsOut(){//judge if the player is out
         if (player.getMoney()<=0){
             player.setIsOut(true);
             Property property=player.getPropertyList();
             int propertyNum=player.getPropertyList().getLandList().size();
 
-            for(int i=0; i<propertyNum; i++){
+            for(int i=0; i<propertyNum; i++){//release the player's property
                 int position=player.getPropertyList().getLandList().get(0);
                 Square square=board.squares[position-1];
                 LandSquare landSquare=(LandSquare) square;
